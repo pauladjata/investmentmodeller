0be49aa846306e91a560e3f710f0f2c70f3e3509
TobyAndTilly <paul.reynolds@adjata.com>
2021-10-20 19:19:09 +1100

    completed code

--- invest.py
+++ invest.py
@@ -6,6 +6,10 @@ from rich import print
 
 st.set_page_config(layout="wide")
 
+# constants
+
+CASH_RETURN = 0.005
+
 
 def invest_strat_chosen_widget(dict):
 
@@ -33,16 +37,18 @@ def investment_horizon_widget():
 
 def investment_period_widget():
 
-    selection = st.radio('Select your investment period', [
+    selection = st.radio('Select the period you make regular contributions', [
         'weekly', 'monthly', 'yearly'])
 
     return selection
 
 
-def investment_amount_widget():
+def investment_amount_widget(contribution_period):
+
+    string_ = f"Select your regular contribution amount for each {contribution_period} ($)"
 
     selection = st.slider(
-        'Select your investment amount for each period ($)', min_value=0, max_value=2000, value=100, step=10, format="$%d")
+        string_, min_value=0, max_value=2000, value=100, step=10, format="$%d per " + contribution_period)
 
     return selection
 
@@ -56,9 +62,13 @@ def get_interest_rate_to_be_used(selection):
             return v['return']
 
 
-def create_chart_data_init(n, initial_investment_amount, periodic_investment_amount, applied_interest_rate):
+def create_chart_data_init(n, initial_investment_amount, periodic_investment_amount, applied_interest_rate, n_periods_per_year):
 
     chart_data_init_dict = {}
+
+    total_funds_invested = 0
+
+    final_ending_principal = 0
 
     for i in range(0, n):
 
@@ -70,11 +80,16 @@ def create_chart_data_init(n, initial_investment_amount, periodic_investment_amo
 
             periodic_investment = 0
 
+            total_funds_invested = total_funds_invested + \
+                initial_investment_amount + periodic_investment
+
         else:
 
             starting_principal = chart_data_init_dict[i-1]['ending_principal']
 
             periodic_investment = periodic_investment_amount
+
+            total_funds_invested = total_funds_invested + periodic_investment
 
         sub_principal_pre_interest = starting_principal + periodic_investment
 
@@ -90,9 +105,13 @@ def create_chart_data_init(n, initial_investment_amount, periodic_investment_amo
             'ending_principal': ending_principal
         }
 
+        final_ending_principal = ending_principal
+
     # create data for dataframe
 
     chart_data_dict = {}
+    compressed_dict = {}
+    counter_ = 0
 
     for i in range(0, n + 1):
 
@@ -106,12 +125,27 @@ def create_chart_data_init(n, initial_investment_amount, periodic_investment_amo
 
             chart_data_dict[i] = chart_data_init_dict[i-1]['ending_principal']
 
-    return chart_data_dict
+        # compressed_dict
+
+    for i in range(0, n + 1):
+
+        if i == 0:
+
+            compressed_dict.setdefault(counter_, [])
+            compressed_dict[counter_] = initial_investment_amount
+
+        elif i % n_periods_per_year == 0:
+
+            counter_ += 1
+
+            compressed_dict.setdefault(counter_, [])
+            compressed_dict[counter_] = chart_data_init_dict[i -
+                                                             1]['ending_principal']
+
+    return chart_data_dict, total_funds_invested, final_ending_principal, compressed_dict
 
 
-CASH_RETURN = 0.005
-
-st.title('Investment Modelling')
+st.header('Investment Modelling')
 
 invest_strat_dict = {
     0: {
@@ -144,33 +178,39 @@ for k, v in invest_strat_dict.items():
 
 # create columns
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
+col4, col5, col6 = st.columns(3)
+
+period_convert_dict = {
+    'weekly': 'week',
+    'monthly': 'month',
+    'yearly': 'year'
+}
+
 
 with col1:
-    st.header("Investment strategy")
+    st.subheader("Investment strategy")
     invest_strat_chosen = invest_strat_chosen_widget(invest_strat_choices_dict)
 
 
 with col2:
-    st.header("Investment horizon")
+    st.subheader("Investment horizon")
     investment_horizon = investment_horizon_widget()
 
-col3, col4 = st.columns(2)
-
 with col3:
-    st.header("Investment period")
+    st.subheader("Initial investment amount")
+    initial_investment_amount = initial_investment_amount_widget()
+
+with col4:
+    st.subheader("Regular contribution period")
     investment_period = investment_period_widget()
 
 
-with col4:
-    st.header("Investment amount for each period")
-    periodic_investment_amount = investment_amount_widget()
-
-col5, col6 = st.columns(2)
-
 with col5:
-    st.header("Initial investment amount")
-    initial_investment_amount = initial_investment_amount_widget()
+    st.subheader("Regular contribution amount for each period")
+    periodic_investment_amount = investment_amount_widget(
+        period_convert_dict[investment_period])
+
 
 # create data for chart
 # assumptions:
@@ -186,21 +226,73 @@ period_n_dict = {
     'yearly': 1
 }
 
+
 # get interest rate to be used
 
 interest_rate = get_interest_rate_to_be_used(invest_strat_chosen)
 
-applied_interest_rate = interest_rate / period_n_dict[investment_period]
+n_periods_per_year = period_n_dict[investment_period]
 
-n = investment_horizon * period_n_dict[investment_period]
+applied_interest_rate = interest_rate / n_periods_per_year
+
+n = investment_horizon * n_periods_per_year
+
+# create data for investment
+x = create_chart_data_init(
+    n, initial_investment_amount, periodic_investment_amount, applied_interest_rate, n_periods_per_year)
+
+chart_data_dict = x[0]
+total_funds_invested = x[1]
+final_principal = x[2]
+compressed_return_data_dict = x[3]
 
 
-chart_data_dict = create_chart_data_init(
-    n, initial_investment_amount, periodic_investment_amount, applied_interest_rate)
+your_investment_return_string = f"Your Investment {(interest_rate * 100):.2f}%"
+
+chart_data_df = pd.DataFrame.from_dict(
+    compressed_return_data_dict, orient='index', columns=[your_investment_return_string])
+
+# create data for cash
+applied_interest_rate = CASH_RETURN / n_periods_per_year
+
+y = create_chart_data_init(
+    n, initial_investment_amount, periodic_investment_amount, applied_interest_rate, n_periods_per_year)
+chart_data_CASH_dict = y[0]
+final_principal_CASH = y[2]
+compressed_return_CASH_data_dict = y[3]
+
+cash_return_string = f"Cash return {(CASH_RETURN * 100):.2f}%"
+
+chart_data_CASH_df = pd.DataFrame.from_dict(
+    compressed_return_CASH_data_dict, orient='index', columns=[cash_return_string])
+
+all_chart_data = pd.concat([chart_data_CASH_df, chart_data_df], axis=1)
+
+# chart_data = pd.DataFrame.from_dict(
+#     all_chart_data, orient='index', columns=['Your Investment'])
+
+return_over_funds_invested = final_principal - total_funds_invested
+return_over_cash = final_principal - final_principal_CASH
+
+print(all_chart_data)
+print(f"total_funds_invested: {total_funds_invested:,}")
+print(f"final_principal: {final_principal:,.2f}")
+print(f"return_over_funds_invested: {return_over_funds_invested:,.2f}")
+print(f"return_over_cash: {return_over_cash:,.2f}")
+print(compressed_return_data_dict)
+
+st.subheader("Your investment returns")
+string_1 = f"your total funds invested: ${total_funds_invested:,}"
+string_2 = f"your total funds returned: ${final_principal:,.2f}"
+string_3 = f"your returns over funds invested: ${return_over_funds_invested:,.2f}"
+# st.write(f"Total funds invested: ${total_funds_invested:,}")
+st.write(f"Your returns: {string_1} || {string_2} || {string_3}")
+
+string_4 = f"total cash returns: ${final_principal_CASH:,.2f}"
+string_5 = f"total cash returns over your funds invested: ${(final_principal_CASH - total_funds_invested):,.2f}"
+string_6 = f"your returns over total cash returns: ${(return_over_cash):,.2f}"
+st.write(
+    f"Your returns compared to cash: {string_4} || {string_5} || {string_6}")
 
 
-chart_data = pd.DataFrame.from_dict(
-    chart_data_dict, orient='index', columns=['Your Investment'])
-
-
-st.line_chart(chart_data)
+st.line_chart(all_chart_data)
