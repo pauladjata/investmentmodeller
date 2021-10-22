0de12c10307d641d42c4b333880ef64edf4de526
TobyAndTilly <paul.reynolds@adjata.com>
2021-10-22 18:03:57 +1100

    created funds contributed line

--- invest_streamlit_app.py
+++ invest_streamlit_app.py
@@ -100,7 +100,8 @@ def create_chart_data_init(n, initial_investment_amount, periodic_investment_amo
             'periodic_investment': periodic_investment,
             'sub_principal_pre_interest': sub_principal_pre_interest,
             'interest': interest_for_period,
-            'ending_principal': ending_principal
+            'ending_principal': ending_principal,
+            'total_funds_invested': total_funds_invested
         }
 
         final_ending_principal = ending_principal
@@ -108,7 +109,8 @@ def create_chart_data_init(n, initial_investment_amount, periodic_investment_amo
     # create data for dataframe
 
     chart_data_dict = {}
-    compressed_dict = {}
+    compressed_investment_dict = {}
+    compressed_funds_employed_dict = {}
     counter_ = 0
 
     for i in range(0, n + 1):
@@ -123,24 +125,29 @@ def create_chart_data_init(n, initial_investment_amount, periodic_investment_amo
 
             chart_data_dict[i] = chart_data_init_dict[i-1]['ending_principal']
 
-        # compressed_dict
+    # compressed_investment_dict - investment amount
+    # compressed_funds_employed_dict - funds employed
 
     for i in range(0, n + 1):
 
+        compressed_investment_dict.setdefault(counter_, [])
+        compressed_funds_employed_dict.setdefault(counter_, [])
+
         if i == 0:
 
-            compressed_dict.setdefault(counter_, [])
-            compressed_dict[counter_] = initial_investment_amount
+            compressed_investment_dict[counter_] = initial_investment_amount
+            compressed_funds_employed_dict[counter_] = initial_investment_amount
 
         elif i % n_periods_per_year == 0:
 
             counter_ += 1
 
-            compressed_dict.setdefault(counter_, [])
-            compressed_dict[counter_] = chart_data_init_dict[i -
-                                                             1]['ending_principal']
+            compressed_investment_dict[counter_] = chart_data_init_dict[i -
+                                                                        1]['ending_principal']
+            compressed_funds_employed_dict[counter_] = chart_data_init_dict[i -
+                                                                            1]['total_funds_invested']
 
-    return chart_data_dict, total_funds_invested, final_ending_principal, compressed_dict
+    return chart_data_dict, total_funds_invested, final_ending_principal, compressed_investment_dict, compressed_funds_employed_dict
 
 
 st.header('Investment Modelling')
@@ -239,11 +246,18 @@ chart_data_dict = x[0]
 total_funds_invested = x[1]
 final_principal = x[2]
 compressed_return_data_dict = x[3]
+compressed_funds_employed_data_dict = x[4]
 
 your_investment_return_string = f"Your Investment {(interest_rate * 100):.2f}%"
+your_funds_employed_string = f"Your Contributions ${total_funds_invested:,}"
 
+# investment strategy return data
 chart_data_df = pd.DataFrame.from_dict(
     compressed_return_data_dict, orient='index', columns=[your_investment_return_string])
+
+# investment strategy return data
+funds_employed_df = pd.DataFrame.from_dict(
+    compressed_funds_employed_data_dict, orient='index', columns=[your_funds_employed_string])
 
 # create data for cash
 applied_interest_rate = CASH_RETURN / n_periods_per_year
@@ -254,12 +268,13 @@ chart_data_CASH_dict = y[0]
 final_principal_CASH = y[2]
 compressed_return_CASH_data_dict = y[3]
 
-cash_return_string = f"Cash return {(CASH_RETURN * 100):.2f}%"
+cash_return_string = f"Cash Return {(CASH_RETURN * 100):.2f}%"
 
 chart_data_CASH_df = pd.DataFrame.from_dict(
     compressed_return_CASH_data_dict, orient='index', columns=[cash_return_string])
 
-all_chart_data = pd.concat([chart_data_CASH_df, chart_data_df], axis=1)
+all_chart_data = pd.concat(
+    [funds_employed_df, chart_data_CASH_df, chart_data_df], axis=1)
 
 
 return_over_funds_invested = final_principal - total_funds_invested
@@ -271,6 +286,7 @@ return_over_cash = final_principal - final_principal_CASH
 # print(f"return_over_funds_invested: {return_over_funds_invested:,.2f}")
 # print(f"return_over_cash: {return_over_cash:,.2f}")
 # print(compressed_return_data_dict)
+# print(funds_employed_df)
 
 st.subheader("Your investment returns")
 string_1 = f"your total funds invested: ${total_funds_invested:,}"
